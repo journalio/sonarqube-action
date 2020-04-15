cbe23e923a56fa60dbaf819559a9a282abe9d163
Wesley Klop <wesley19097@gmail.com>
2020-04-15 13:35:34 +0200

    Add project key option and force scm provider git

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,12 @@ fi
 
 sonar-scanner \
 	-Dsonar.host.url=${INPUT_HOST} \
-	-Dsonar.projectKey=${PWD##*/} \
+	-Dsonar.projectKey=${INPUT_PROJECTKEY} \
 	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
 	-Dsonar.login=${INPUT_LOGIN} \
 	-Dsonar.password=${INPUT_PASSWORD} \
 	-Dsonar.sources=. \
 	-Dsonar.tests=. \
 	-Dsonar.sourceEncoding=UTF-8 \
+	-Dsonar.scm.provider=git \
 	${SONAR_PASSWORD}
